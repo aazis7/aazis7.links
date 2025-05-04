a95f3e665ec48e13183ced0436012dedde4d93c7
aazis7 <azis190407@gmail.com>
2025-05-04 16:01:33 +0700

    feat: og

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,4 @@ export const SITE_DESCRIPTION =
     'Fresh graduate with high learning spirit, focus on web development, and web design. Currently building independent projects while actively seeking job opportunities and collaborations.';
 export const SITE_FAVICON = '/favicon.ico';
 export const SITE_APPLE_ICON = '/apple-touch-icon.png';
-export const SITE_OG_IMAGE = '/open-graph-image.png';
+export const SITE_OG_IMAGE = `${process.env.NEXT_PUBLIC_BASE_URL}/api/og`;
